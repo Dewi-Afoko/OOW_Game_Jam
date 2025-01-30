bb3b8593dc5f6f41a084a375f0078d69d694c12c
Dewi Afoko <dewi.afoko@gmail.com>
2025-01-30 15:36:29 +0000

    sfx

--- game.py
+++ game.py
@@ -3,9 +3,13 @@ import random
 
 # Initialize Pygame
 pygame.init()
+pygame.mixer.init()
+
+pygame.mixer.music.load('./assets/soundtrack.mp3')
+pygame.mixer.music.set_volume(0.5)
+pygame.mixer.music.play(-1, 0.0)
 
 # Game window settings
-
 
 WIDTH, HEIGHT = 1440, 1080
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -57,18 +61,19 @@ winning_pollution_level = 0
 losing_pollution_level = 100
 game_over = False
 
+clock = pygame.time.Clock()
 
 # Game loop
 running = True
 while running:
     screen.blit(background, (0,0)) 
-
+    clock.tick(60)
     if not game_over:
 
         current_pollution_level = pygame.time.get_ticks()
 
         if 0 < (current_pollution_level - pollution_level) > 100:
-            pollution_level += 0.1
+            pollution_level += 0.017
             last_pollution_update = current_pollution_level 
 
         # Draw trash from trash_list 
@@ -80,7 +85,7 @@ while running:
             screen.blit(TURTLE_IMAGE, (turtle.x, turtle.y))
 
         # Display pollution_level
-        pollution_text = font.render(f"Pollution level: {pollution_level}", True, WHITE)
+        pollution_text = font.render(f"Pollution level: {int(pollution_level)}", True, WHITE)
         screen.blit(pollution_text, (20, 20))
 
         # Event handling
@@ -91,24 +96,24 @@ while running:
             if event.type == pygame.MOUSEBUTTONDOWN:  # Detect mouse click
                 for trash in trash_list[:]: #Copy trash_list to maintain range/spawn number
                     if trash.collidepoint(event.pos):  # If clicked on trash
-                        pollution_level -= 3
+                        pollution_level -= 5
                         trash_list.remove(trash)  # Delete clicked trash
                         trash_list.append(spawn_trash()) #Spawn new trash
 
             if event.type == pygame.MOUSEMOTION:  # Detect mouse moving over turtle
                 for turtle in turtle_list[:]: #Copy trash_list to maintain range/spawn number
                     if turtle.collidepoint(event.pos):  # If clicked on trash
-                        pollution_level += 5
+                        pollution_level += 10
                         turtle_list.remove(turtle)  # Delete clicked trash
                         turtle_list.append(spawn_turtle()) #Spawn new trash
 
         if pollution_level < 30:
             keep_going = font.render("Great job, keep going!", True, WHITE)
-            screen.blit(keep_going, (400, 300))
+            screen.blit(keep_going, (WIDTH // 2 - 200, HEIGHT // 2 - 50))
 
         if pollution_level > 70:
             turtle_killer = font.render("Try and clean up the trash, while avoiding turtles!", True, RED)
-            screen.blit(turtle_killer, (250, 250))
+            screen.blit(turtle_killer, (WIDTH // 2 - 200, HEIGHT // 2 - 50))
 
         if pollution_level <= winning_pollution_level or pollution_level >= losing_pollution_level:
                 game_over = True
@@ -117,7 +122,7 @@ while running:
         winning_message = alert_font.render("You win!", True, (255, 255, 0))
         screen.blit(winning_message, (WIDTH // 2 - 200, HEIGHT // 2 - 50))
 
-        restart_text = font.render("Click or press any key to restart", True, (255, 255, 255))
+        restart_text = font.render("Click or press any key to restart on hard mode", True, (255, 255, 255))
         screen.blit(restart_text, (WIDTH // 2 - 200, HEIGHT // 2 + 50))
 
         for event in pygame.event.get():
@@ -133,7 +138,7 @@ while running:
         winning_message = alert_font.render("You lose!", True, (255, 255, 0))
         screen.blit(winning_message, (WIDTH // 2 - 200, HEIGHT // 2 - 50))
 
-        restart_text = font.render("Click or press any key to restart", True, (255, 255, 255))
+        restart_text = font.render("Click or press any key to restart on easy mode", True, (255, 255, 255))
         screen.blit(restart_text, (WIDTH // 2 - 200, HEIGHT // 2 + 50))
 
         for event in pygame.event.get():
@@ -142,11 +147,12 @@ while running:
             elif event.type in (pygame.MOUSEBUTTONDOWN, pygame.KEYDOWN):  # Restart game on click or key press
                 pollution_level = 50
                 game_over = False
-                trash_list = [spawn_trash() for _ in range(4)]
-                turtle_list = [spawn_turtle() for _ in range(4)]
+                trash_list = [spawn_trash() for _ in range(6)]
+                turtle_list = [spawn_turtle() for _ in range(2)]
         
 
 
     pygame.display.flip()  # Update display
 
+pygame.mixer.music.stop()
 pygame.quit()
